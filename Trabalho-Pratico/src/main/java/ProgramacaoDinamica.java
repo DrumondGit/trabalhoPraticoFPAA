79c3b92f21c2c8985bdc1851088b0137ce606210
PedroRamos111 <104322803+PedroRamos111@users.noreply.github.com>
2023-11-26 15:40:20 -0300

    pd

--- Trabalho-Pratico/src/main/java/ProgramacaoDinamica.java
+++ Trabalho-Pratico/src/main/java/ProgramacaoDinamica.java
@@ -1,40 +1,67 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class ProgramacaoDinamica {
-    static int INF = 1000000007;
+    static int INF = Integer.MAX_VALUE;
     static int[][] teste;
     static int[] teste3;
     static int[][] teste2;
 
-    public static void programacaoDinamica(String[] args) {
-        int[] rotas = {35, 34, 33, 23, 21, 20, 30};
+    public static void inicio(String[] args) {
         int N = 3;
-        Arrays.sort(rotas);
-        teste3 = new int[rotas.length+1];
-        for (int i = 1; i <= rotas.length; i++) {
-            teste3[i] = teste3[i-1] + rotas[i-1];
-        }
-        teste = new int[N+1][rotas.length+1];
-        teste2 = new int[N+1][rotas.length+1];
-        for (int[] row : teste) {
-            Arrays.fill(row, 0);
-        }
-        solve(N, rotas.length);
         
+        List<int[]> conjuntoDeTeste = GeradorDeProblemas.geracaoDeRotas(30, 10, 0.5);
+        for (int[] rotas : conjuntoDeTeste) {
+            for(int i = 0; i < rotas.length; i++){
+
+                System.out.print(rotas[i] + " ");
+                
+            }
+            System.out.println();
+            pd(rotas, N);
+        }
+    }
+
+    public static void pd(int[] rotas, int N) {
+        Arrays.sort(rotas);
+        teste3 = new int[rotas.length + 1];
+        for (int i = 1; i <= rotas.length; i++) {
+            teste3[i] = teste3[i - 1] + rotas[i - 1];
+        }
+        teste = new int[N + 1][rotas.length + 1];
+        teste2 = new int[N + 1][rotas.length + 1];
+        solve(N, rotas.length);
+
         int[] trucks = new int[N];
         int routes = rotas.length;
         for (int i = N; i > 0; i--) {
-            trucks[i-1] = teste3[routes] - teste3[teste2[i][routes]];
+            trucks[i - 1] = teste3[routes] - teste3[teste2[i][routes]];
             routes = teste2[i][routes];
         }
         System.out.println("A quilometragem de cada caminhão é: " + Arrays.toString(trucks));
 
-        for(int i = 0; i < N+1; i++){
-            for(int j = 0; j < rotas.length+1; j++){
-                System.out.print(teste2[i][j] + " ");
-            }
-            System.out.println();
-        }
+        // System.out.println("Teste:");
+        // for (int i = 0; i < N + 1; i++) {
+        //     for (int j = 0; j < rotas.length + 1; j++) {
+        //         System.out.print(teste[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("Teste2:");
+        // for (int i = 0; i < N + 1; i++) {
+        //     for (int j = 0; j < rotas.length + 1; j++) {
+        //         System.out.print(teste2[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // System.out.println("Teste3:");
+        // for (int i = 0; i < rotas.length + 1; i++) {
+
+        //     System.out.print(teste3[i] + " ");
+
+        // }
     }
 
     static int solve(int trucks, int routes) {
@@ -49,7 +76,7 @@ public class ProgramacaoDinamica {
         }
         int ans = INF;
         for (int i = 0; i < routes; i++) {
-            int temp = Math.max(solve(trucks-1, i), teste3[routes] - teste3[i]);
+            int temp = Math.max(solve(trucks - 1, i), teste3[routes] - teste3[i]);
             if (temp < ans) {
                 ans = temp;
                 teste2[trucks][routes] = i;
