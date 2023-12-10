580e4b44bf372c43ae2d985e3f23f21ce6b58027
Nathália Rezende <102332869+nathCota@users.noreply.github.com>
2023-12-10 10:18:52 -0300

    Update EstrategiaGulosa.java

--- Trabalho-Pratico/src/main/java/EstrategiaGulosa.java
+++ Trabalho-Pratico/src/main/java/EstrategiaGulosa.java
@@ -1,29 +1,40 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class EstrategiaGulosa {
 
 
     /**
-     * Realiza testes para avaliar o desempenho de duas estratégias gulosas na distribuição
-     * de rotas para caminhões, comparando a média das quilometragens resultantes.
+     * Testa as estratégias EG1 e EG2 em conjuntos de teste de diferentes tamanhos.
      *
-     * @param Conjunto Um array de inteiros representando os tamanhos dos conjuntos de testes.
+     * Este método realiza testes para avaliar o desempenho das estratégias Gulosa 1 (EG1) e
+     * Gulosa 2 (EG2) em conjuntos de teste de diferentes tamanhos. A média da quilometragem total
+     * é calculada para cada estratégia ao longo de vários testes.
+     *
+     * @param tamanhosConjunto Um array de inteiros representando os tamanhos dos conjuntos de teste.
      */
-    public static void testarEstrategias(int[] Conjunto) {
+    public static void testarEstrategias( int[] tamanhosConjunto) {
+        int numCaminhoes = 3;
         int numTestes = 10;
-        int num_caminhoes = 3;
 
-        for (int tamanho : Conjunto) {
-            int quantRotas = tamanho / 2;
-            List<int[]> conjuntoDeTeste = GeradorDeProblemas.geracaoDeRotas(quantRotas, tamanho, 0.5);
-
+        for (int tamanhoConjunto : tamanhosConjunto) {
             double mediaEG1 = 0;
             double mediaEG2 = 0;
+            int []  quilometragens;
 
             for (int teste = 0; teste < numTestes; teste++) {
-                int[][] distribuicaoEG1 = distribuirRotas(conjuntoDeTeste, num_caminhoes, "EG1");
-                int[][] distribuicaoEG2 = distribuirRotas(conjuntoDeTeste, num_caminhoes, "EG2");
+                List<int[]> conjuntoDeTeste = GeradorDeProblemas.geracaoDeRotas(tamanhoConjunto / 2, tamanhoConjunto, 1.0);
+
+
+                System.out.println("\nEstratégia Gulosa 1:");
+                quilometragens = conjuntoDeTeste(conjuntoDeTeste,"EG1");
+                int[][] distribuicaoEG1 = distribuirRotas(quilometragens, numCaminhoes, "EG1");
+
+                System.out.println("\nEstratégia Gulosa 2:");
+                quilometragens = conjuntoDeTeste(conjuntoDeTeste,"EG2");
+                int[][] distribuicaoEG2 = distribuirRotas(quilometragens, numCaminhoes, "EG2");
 
                 mediaEG1 += calcularQuilometragemTotal(distribuicaoEG1);
                 mediaEG2 += calcularQuilometragemTotal(distribuicaoEG2);
@@ -32,22 +43,123 @@ public class EstrategiaGulosa {
             mediaEG1 /= numTestes;
             mediaEG2 /= numTestes;
 
-            System.out.println("Tamanho do Conjunto: " + tamanho);
+            System.out.println("\n" + "Tamanho do Conjunto: " + tamanhoConjunto);
             System.out.println("Média EG1: " + mediaEG1);
             System.out.println("Média EG2: " + mediaEG2);
             System.out.println();
-
         }
-
     }
 
 
     /**
-     * Calcula a quilometragem total de uma distribuição de rotas para caminhões.
+     * Seleciona e retorna um conjunto de teste com base em uma estratégia específica.
      *
-     * @param distribuicao Uma matriz representando a distribuição de rotas para caminhões,
-     *                     onde cada linha representa um caminhão e cada coluna representa uma rota.
-     * @return A quilometragem total da distribuição de rotas.
+     * Este método recebe uma lista de conjuntos de teste e uma estratégia para selecionar um conjunto.
+     * Os conjuntos de teste são representados como arrays de inteiros, onde cada elemento contém informações
+     * relevantes para o teste. A estratégia determina a ordenação da lista antes de selecionar o conjunto.
+     *
+     * @param conjuntoDeTeste Uma lista de arrays de inteiros representando conjuntos de teste.
+     * @param estrategia A estratégia de seleção de conjunto a ser utilizada ("EG1" ou "EG2").
+     * @return Um array de inteiros representando o conjunto de teste selecionado de acordo com a estratégia.
+     *         Retorna null se a estratégia não for reconhecida.
+     */
+    private static int[] conjuntoDeTeste( List<int[]> conjuntoDeTeste, String estrategia) {
+        if ("EG1".equals(estrategia)) {
+            conjuntoDeTeste.sort(Comparator.comparingInt(a -> a[0]));
+            return conjuntoDeTeste.get(0);
+        } else if ("EG2".equals(estrategia)) {
+            conjuntoDeTeste.sort(Comparator.comparingInt(a -> -a[0]));
+            return conjuntoDeTeste.get(0);
+        }
+        return null;
+    }
+
+
+    /**
+     * Distribui as rotas entre os caminhões com base em uma estratégia específica.
+     *
+     * Este método recebe um array de quilometragens, o número de caminhões e uma estratégia
+     * para distribuir as rotas entre os caminhões. Os caminhões são representados por uma matriz
+     * onde cada coluna representa um caminhão e as rotas são distribuídas de acordo com a estratégia.
+     *
+     * @param quilometragens Um array de inteiros representando as quilometragens a serem distribuídas.
+     * @param numCaminhoes O número de caminhões disponíveis.
+     * @param estrategia A estratégia de distribuição de rotas a ser utilizada ("EG1" ou "EG2").
+     * @return Uma matriz representando a distribuição das rotas entre os caminhões.
+     */
+    public static int[][] distribuirRotas(int[] quilometragens, int numCaminhoes, String estrategia) {
+        int[][] caminhoes = new int[numCaminhoes][quilometragens.length / 2];
+        Arrays.sort(quilometragens);
+
+        if ("EG1".equals(estrategia)) {
+            distribuirEG1(quilometragens, caminhoes);
+        } else if ("EG2".equals(estrategia)) {
+            distribuirEG2(quilometragens, caminhoes);
+        }
+
+        imprimirDistribuicao(caminhoes);
+        return caminhoes;
+    }
+
+    /**
+     * Distribui as quilometragens entre os caminhões de forma crescente.
+     *
+     * Este método recebe um array de quilometragens e uma matriz de caminhões.
+     * As quilometragens são distribuídas entre os caminhões, preenchendo cada coluna
+     * com os valores correspondentes em ordem crescente.
+     *
+     * @param quilometragens Um array de inteiros representando as quilometragens a serem distribuídas.
+     * @param caminhoes Uma matriz onde cada coluna representa um caminhão e
+     *                  as quilometragens são distribuídas de forma crescente em cada coluna.
+     */
+    private static void distribuirEG1(int[] quilometragens, int[][] caminhoes) {
+        List<Integer> quilometragensList = Arrays.stream(quilometragens).boxed().collect(Collectors.toList());
+        quilometragensList.sort(Comparator.naturalOrder());
+        int i = 0;
+        for (int c = 0; c < caminhoes[0].length; c++) {
+            for (int l = 0; l < caminhoes.length; l++) {
+                if (i == quilometragens.length) {
+                    break;
+                }
+                caminhoes[l][c] = quilometragensList.get(i);
+                i++;
+            }
+        }
+    }
+
+
+    /**
+     * Distribui as quilometragens entre os caminhões de forma decrescente.
+     *
+     * Este método recebe um array de quilometragens e uma matriz de caminhões.
+     * As quilometragens são distribuídas entre os caminhões, preenchendo cada coluna
+     * com os valores correspondentes em ordem decrescente.
+     *
+     * @param quilometragens Um array de inteiros representando as quilometragens a serem distribuídas.
+     * @param caminhoes Uma matriz onde cada coluna representa um caminhão e
+     *                  as quilometragens são distribuídas de forma decrescente em cada coluna.
+     */
+    private static void distribuirEG2(int[] quilometragens, int[][] caminhoes) {
+        List<Integer> quilometragensList = Arrays.stream(quilometragens).boxed().collect(Collectors.toList());
+        quilometragensList.sort(Comparator.reverseOrder());
+        int i = 0;
+        for (int c = 0; c < caminhoes[0].length; c++) {
+            for (int l = 0; l < caminhoes.length; l++) {
+                if (i == quilometragens.length) {
+                    break;
+                }
+                caminhoes[l][c] = quilometragensList.get(i);
+                i++;
+            }
+        }
+    }
+
+
+    /**
+     * Calcula e retorna a quilometragem total percorrida com base na matriz de distribuição.
+     *
+     * @param distribuicao Uma matriz 2D onde cada elemento representa a quilometragem percorrida.
+     * @return A quilometragem total percorrida pelos caminhões.
      */
     private static double calcularQuilometragemTotal(int[][] distribuicao) {
         double total = 0;
@@ -61,74 +173,9 @@ public class EstrategiaGulosa {
 
 
     /**
-     * Distribui rotas entre caminhões utilizando uma estratégia gulosa especificada.
+     * Gera e retorna um conjunto de números inteiros com base no valor inicial fornecido.
      *
-     * @param rotas      Uma lista de inteiros representando as quilometragens das rotas.
-     * @param numCaminhoes O número de caminhões disponíveis para a distribuição.
-     * @param estrategia A estratégia gulosa a ser utilizada ("EG1" ou "EG2").
-     * @return Uma matriz representando a distribuição de rotas entre os caminhões.
-     */
-    private static int[][] distribuirRotas(List<int[]> rotas, int numCaminhoes, String estrategia) {
-        int[][] distribuicao = new int[numCaminhoes][rotas.get(0).length];
-
-        if ("EG1".equals(estrategia)) {
-            estrategiaGulosa1(rotas, numCaminhoes, distribuicao);
-        } else if ("EG2".equals(estrategia)) {
-            estrategiaGulosa2(rotas, numCaminhoes, distribuicao);
-        }
-
-        return distribuicao;
-    }
-
-
-    /**
-     * Aplica a Estratégia Gulosa 1 para distribuição de rotas entre caminhões.
-     * A estratégia consiste em ordenar as rotas em ordem decrescente de quilometragem
-     * e atribuir cada rota ao caminhão com a menor quilometragem acumulada até o momento.
-     *
-     * @param rotas         Uma lista de inteiros representando as quilometragens das rotas.
-     * @param numCaminhoes  O número de caminhões disponíveis para a distribuição.
-     * @param distribuicao  Uma matriz representando a distribuição de rotas entre os caminhões.
-     */
-    private static void estrategiaGulosa1(List<int[]> rotas, int numCaminhoes, int[][] distribuicao) {
-        rotas.sort((a, b) -> Integer.compare(b[0], a[0]));
-
-        for (int j = 0; j < rotas.get(0).length; j++) {
-            for (int i = 0; i < numCaminhoes; i++) {
-                if (distribuicao[i][j] == 0) {
-                    distribuicao[i][j] = rotas.get(j)[i];
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * Aplica a Estratégia Gulosa 2 para distribuição de rotas entre caminhões.
-     * A estratégia consiste em ordenar as rotas em ordem crescente de quilometragem
-     * e atribuir cada rota ao caminhão com a menor quilometragem acumulada até o momento.
-     *
-     * @param rotas         Uma lista de inteiros representando as quilometragens das rotas.
-     * @param numCaminhoes  O número de caminhões disponíveis para a distribuição.
-     * @param distribuicao  Uma matriz representando a distribuição de rotas entre os caminhões.
-     */
-    private static void estrategiaGulosa2(List<int[]> rotas, int numCaminhoes, int[][] distribuicao) {
-        rotas.sort(Comparator.comparingInt(a -> a[0]));
-
-        for (int j = 0; j < rotas.get(0).length; j++) {
-            for (int i = 0; i < numCaminhoes; i++) {
-                if (distribuicao[i][j] == 0) {
-                    distribuicao[i][j] = rotas.get(j)[i];
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * Gera um conjunto de inteiros com base em um valor inicial, aumentando-o em incrementos fixos.
-     *
-     * @param init O valor inicial para o primeiro elemento do conjunto.
+     * @param init O valor inicial a ser usado para gerar o conjunto.
      * @return Um array de inteiros representando o conjunto gerado.
      */
     public static int[] gerarConjunto(int init) {
@@ -138,5 +185,31 @@ public class EstrategiaGulosa {
             init += 19;
         }
         return conjunto;
+    }
+
+    /**
+     * Imprime a distribuição das distâncias percorridas por cada caminhão.
+     *
+     * @param caminhoes Uma matriz 2D onde cada linha representa um caminhão
+     *                  e cada coluna representa a distância percorrida em uma rota.
+     */
+    private static void imprimirDistribuicao(int[][] caminhoes) {
+        for (int l = 0; l < caminhoes.length; l++) {
+            System.out.print("Caminhão " + (l + 1) + ": rotas ");
+            int totalKm = 0;
+
+            for (int c = 0; c < caminhoes[l].length; c++) {
+                if (caminhoes[l][c] != 0) {
+                   // System.out.print(caminhoes[l][c]);
+                    totalKm += caminhoes[l][c];
+
+                    if (c < caminhoes[l].length - 1 && caminhoes[l][c + 1] != 0) {
+                      //  System.out.print(", ");
+                    }
+                }
+            }
+
+            System.out.println("  total " + totalKm + "km");
+        }
     }
 }
